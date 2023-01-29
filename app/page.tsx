75d22356e44066400ac86ee16d4aac8885aa1c62
Sovannborith Yun <yun.sovannborith@gmail.com>
2023-01-29 16:12:16 +0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to Borith's Blog</h1>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ borith-blog/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'lxr5ivkh',
+    dataset: 'production'
+  }
+})
